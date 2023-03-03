1055ff94e84dde06bab610340f8847f98666e1db
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-03-03 22:09:54 +0900

    loginTest.js proceeding

--- src/components/LoginTest.js
+++ src/components/LoginTest.js
@@ -53,6 +53,9 @@ function LoginTest() {
             let loginCheckState = checkLogin();
             if ( loginCheckState ) {
                 console.log('성공');
+
+
+
                 // axios.post('/user', {
                 //     id: enteredId,
                 //     password: enteredPw
@@ -63,6 +66,49 @@ function LoginTest() {
                 //   .catch(function (error) {
                 //     console.log(error);
                 //   });
+
+
+                const axios = require('axios');
+
+                // 지정된 ID를 가진 유저에 대한 요청
+                axios.get('/user?ID=12345')
+                .then(function (response) {
+                    // 성공 핸들링
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    // 에러 핸들링
+                    console.log(error);
+                })
+                .finally(function () {
+                    // 항상 실행되는 영역
+                });
+
+                // 선택적으로 위의 요청은 다음과 같이 수행될 수 있습니다.
+                axios.get('/user', {
+                    params: {
+                    ID: 12345
+                    }
+                })
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .finally(function () {
+                    // 항상 실행되는 영역
+                });  
+
+                // async/await 사용을 원한다면, 함수 외부에 `async` 키워드를 추가하세요.
+                async function getUser() {
+                try {
+                    const response = await axios.get('/user?ID=12345');
+                    console.log(response);
+                } catch (error) {
+                    console.error(error);
+                }
+                }
             }
         }
 
